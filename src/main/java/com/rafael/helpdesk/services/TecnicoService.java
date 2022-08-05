36bc3dddb51b08ec36719e68daf967a02e5ae422
Rafael Moreira <rafaelmoreira.at@hotmail.com>
2022-08-05 10:54:43 -0400

    endpoint Create para Tecnico

--- src/main/java/com/rafael/helpdesk/services/TecnicoService.java
+++ src/main/java/com/rafael/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafael.helpdesk.domain.Tecnico;
+import com.rafael.helpdesk.domain.dtos.TecnicoDTO;
 import com.rafael.helpdesk.repositories.TecnicoRepository;
 import com.rafael.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -23,5 +24,11 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 }
