4baf53e3bfb1bd04d0eeac479c5be27d8d9481bc
Rafael Moreira <rafaelmoreira.at@hotmail.com>
2022-08-04 21:45:49 -0400

    Anotações JPA para Classes de Domínio - Aula 3

--- src/main/java/com/rafael/helpdesk/domain/Chamado.java
+++ src/main/java/com/rafael/helpdesk/domain/Chamado.java
@@ -1,22 +1,43 @@
 package com.rafael.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.rafael.helpdesk.domain.enums.Prioridade;
 import com.rafael.helpdesk.domain.enums.Status;
 
-public class Chamado {
+@Entity
+public class Chamado implements Serializable{
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataAbertura = LocalDate.now();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
+	
 	private Prioridade prioridade;
 	private Status status;
 	private String titulo;
 	private String observacoes;
 
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 
 	public Chamado() {
