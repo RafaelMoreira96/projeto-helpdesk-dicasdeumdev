808701a832cb3fc41f44ed8d646ddeb5aac644a0
Rafael Moreira <rafaelmoreira.at@hotmail.com>
2022-08-05 13:27:47 -0400

    Validação nos campos de criação de novo usuario

--- src/main/java/com/rafael/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/rafael/helpdesk/domain/dtos/TecnicoDTO.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.rafael.helpdesk.domain.Tecnico;
 import com.rafael.helpdesk.domain.enums.Perfil;
@@ -14,9 +16,14 @@ public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	protected Integer id;
+	
+	@NotNull(message = "O campo NOME é requerido.")
 	protected String nome;
+	@NotNull(message = "O campo CPF é requerido.")
 	protected String cpf;
+	@NotNull(message = "O campo EMAIL é requerido.")
 	protected String email;
+	@NotNull(message = "O campo SENHA é requerido.")
 	protected String senha;
 	protected Set<Integer> perfis = new HashSet<>();
 
--- src/main/java/com/rafael/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/rafael/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,6 +15,7 @@ import com.rafael.helpdesk.services.exceptions.ObjectnotFoundException;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
+	// Object Not Found Exception
 	@ExceptionHandler(ObjectnotFoundException.class)
 	public ResponseEntity<StandardError> objetcnotFoundException(ObjectnotFoundException ex,
 			HttpServletRequest request) {
@@ -22,7 +25,8 @@ public class ResourceExceptionHandler {
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
-	
+
+	// Data Integrity Violation Exception
 	@ExceptionHandler(DataIntegrityViolationException.class)
 	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
 			HttpServletRequest request) {
@@ -31,6 +35,20 @@ public class ResourceExceptionHandler {
 				"Violação de dados", ex.getMessage(), request.getRequestURI());
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+
+	// Method Argument Not Valid Exception
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException ex,
+			HttpServletRequest request) {
+
+		ValidationError errors = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Validation Error", "Erro na validação dos campos", request.getRequestURI());
+
+		for (FieldError x : ex.getBindingResult().getFieldErrors()) {
+			errors.addError(x.getField(), x.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 	}
 
 }
