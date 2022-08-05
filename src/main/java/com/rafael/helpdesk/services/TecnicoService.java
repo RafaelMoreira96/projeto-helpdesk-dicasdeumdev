9e1f188b17ddd8a596d42f9a16ac3154b4bc2ca1
Rafael Moreira <rafaelmoreira.at@hotmail.com>
2022-08-05 09:02:36 -0400

    endpoint para findById para tecnico

---
+++ src/main/java/com/rafael/helpdesk/services/TecnicoService.java
@@ -0,0 +1,21 @@
+package com.rafael.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafael.helpdesk.domain.Tecnico;
+import com.rafael.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+	
+	@Autowired
+	private TecnicoRepository repository;
+	
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+}
